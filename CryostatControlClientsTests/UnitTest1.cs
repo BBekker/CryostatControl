5f55cc64439c4ed2c452ea61cc4dfd01d2cd6a07
RubenMeeuw <r.d.meeuwissen@student.tudelft.nl>
2017-05-08 16:45:41 +0200

    Simple test for CI testing

---
+++ CryostatControlClientsTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CryostatControlClient;
+
+namespace CryostatControlClientsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var homeScreen = new CryostatControlClient.Models.HomeScreen();
+            Assert.AreEqual(2, homeScreen.simpleCalc());
+        }
+    }
+}
