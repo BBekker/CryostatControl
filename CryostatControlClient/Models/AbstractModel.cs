6cc45dc8280acddc7e25a384d393f30731af9351
lsikkes <louis-sikkes@live.nl>
2017-05-10 13:09:40 +0200

    Made he7model + abstractmodel.

---
+++ CryostatControlClient/Models/AbstractModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryostatControlClient.Models
+{
+    public abstract class AbstractModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
