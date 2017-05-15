37c48b43a07a0380ccd71e1e0d619d3f4885ff72
RubenMeeuw <r.d.meeuwissen@student.tudelft.nl>
2017-05-15 13:01:55 +0200

    Creation of abstractViewModel

---
+++ CryostatControlClient/ViewModels/AbstractViewModel.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AbstractViewModel.cs" company="SRON">
+//   k
+// </copyright>
+// <summary>
+//   The abstract view model.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CryostatControlClient.ViewModels
+{
+    using System.Collections.Specialized;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// The abstract view model.
+    /// </summary>
+    public abstract class AbstractViewModel : INotifyCollectionChanged
+    {
+
+        /// <summary>
+        /// The property changed.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// The collection changed.
+        /// </summary>
+        public abstract event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        /// <summary>
+        /// The raise property changed.
+        /// </summary>
+        /// <param name="propertyName">
+        /// The property name.
+        /// </param>
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ CryostatControlClient/ViewModels/He7ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryostatControlClient.ViewModels
+{
+    using System.Collections.Specialized;
+
+    public class He7ViewModel : AbstractViewModel
+    {
+        public override event NotifyCollectionChangedEventHandler CollectionChanged;
+    }
+}
