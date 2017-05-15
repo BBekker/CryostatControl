e3a809bf88d2854669672536f5c7532a28f8742c
lsikkes <louis-sikkes@live.nl>
2017-05-15 15:51:03 +0200

    Binding first version.

--- CryostatControlClient/Views/MainWindow.xaml.cs
+++ CryostatControlClient/Views/MainWindow.xaml.cs
@@ -9,12 +9,36 @@
 
 namespace CryostatControlClient.Views
 {
+    using System;
     using System.Windows;
+    using System.Windows.Controls;
+
+    using CryostatControlClient.ViewModels;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public MainWindow()
+        {
+            this.Loaded += this.MainWindow_Loaded;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow" /> class.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            BlueforsViewModel dc = new BlueforsViewModel();
+
+            this.DataContext = dc;
+
+            dc.ColdPlate3KTemp = 3000;
+
+            Console.WriteLine(dc.ColdPlate3KTemp);
+        }
     }
 }
