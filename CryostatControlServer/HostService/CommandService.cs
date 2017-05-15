ecae4b6e6044ce3b59e4df55e80bb7371e41d67f
Maiko <M.Goudriaan-2@student.tudelft.nl>
2017-05-15 13:26:40 +0200

    added all files

---
+++ CryostatControlServer/HostService/CommandService.cs
@@ -0,0 +1,17 @@
+﻿namespace CryostatControlServer.HostService
+{
+    using System;
+
+    public class CommandService : ICommandService
+    {
+        #region Methods
+
+        public string SayHello(string name)
+        {
+            Console.WriteLine("hello called");
+            return "hello " + name;
+        }
+
+        #endregion Methods
+    }
+}
---
+++ CryostatControlServer/HostService/ICommandService.cs
@@ -0,0 +1,15 @@
+﻿namespace CryostatControlServer.HostService
+{
+    using System.ServiceModel;
+
+    [ServiceContract]
+    public interface ICommandService
+    {
+        #region Methods
+
+        [OperationContract]
+        string SayHello(string name);
+
+        #endregion Methods
+    }
+}
