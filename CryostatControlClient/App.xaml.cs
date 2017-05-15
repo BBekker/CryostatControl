3fdf9a2148eafcf05c959bd56437c5599e61dd5a
Maiko <M.Goudriaan-2@student.tudelft.nl>
2017-05-15 13:26:18 +0200

    Hello world call implemented

--- CryostatControlClient/App.xaml.cs
+++ CryostatControlClient/App.xaml.cs
@@ -9,12 +9,27 @@
 
 namespace CryostatControlClient
 {
+    using System;
     using System.Windows;
+
+    using CryostatControlClient.ServiceReference1;
 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        #region Methods
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            CommandServiceClient client = new CommandServiceClient();
+
+            Console.WriteLine("{0}", client.SayHello("Maiko"));
+        }
+
+        #endregion Methods
     }
 }
--- CryostatControlServer/Launcher.cs
+++ CryostatControlServer/Launcher.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CryostatControlServer
+﻿namespace CryostatControlServer
 {
-    class Launcher
+    using System;
+    using System.ServiceModel;
+    using System.ServiceModel.Description;
+
+    using CryostatControlServer.HostService;
+
+    public class Launcher
     {
+        #region Methods
+
+        public static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:8080/SRON");
+
+            // Create the ServiceHost.
+            using (ServiceHost host = new ServiceHost(typeof(CommandService), baseAddress))
+            {
+                // Enable metadata publishing.
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                smb.MetadataExporter.PolicyVersion = PolicyVersion.Policy15;
+                host.Description.Behaviors.Add(smb);
+
+                // Open the ServiceHost to start listening for messages. Since
+                // no endpoints are explicitly configured, the runtime will create
+                // one endpoint per base address for each service contract implemented
+                // by the service.
+                host.Open();
+
+                Console.WriteLine("The service is ready at {0}", baseAddress);
+                Console.WriteLine("Press <Enter> to stop the service.");
+                Console.ReadLine();
+
+                // Close the ServiceHost.
+                host.Close();
+            }
+        }
+
+        #endregion Methods
     }
 }
