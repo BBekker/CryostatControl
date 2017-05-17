3617990b0af5de2b67ed765ccc11cc5431ca3cbd
Maiko <M.Goudriaan-2@student.tudelft.nl>
2017-05-17 15:50:28 +0200

    add empty sensor file

---
+++ CryostatControlServer/EmptySensor.cs
@@ -0,0 +1,30 @@
+﻿namespace CryostatControlServer
+{
+    public class EmptySensor : ISensor
+    {
+        #region Properties
+
+        public int Interval
+        {
+            get
+            {
+                return int.MinValue;
+            }
+
+            set
+            {
+                //do nothing
+            }
+        }
+
+        public double Value
+        {
+            get
+            {
+                return double.MinValue;
+            }
+        }
+
+        #endregion Properties
+    }
+}
