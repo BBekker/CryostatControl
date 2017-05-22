2256622f903879d3096b35c216723da5cbcf40a8
lsikkes <louis-sikkes@live.nl>
2017-05-22 15:45:42 +0200

    first version graphs.

--- CryostatControlClient/ViewModels/ViewModelContainer.cs
+++ CryostatControlClient/ViewModels/ViewModelContainer.cs
@@ -13,7 +13,12 @@ namespace CryostatControlClient.ViewModels
     using System.ComponentModel;
     using System.Linq;
     using System.Text;
+    using System.Threading;
     using System.Threading.Tasks;
+
+    using LiveCharts;
+    using LiveCharts.Defaults;
+    using LiveCharts.Wpf;
 
     /// <summary>
     /// The data context.
@@ -42,6 +47,16 @@ namespace CryostatControlClient.ViewModels
         /// </summary>
         private ModusViewModel modusViewModel;
 
+        /// <summary>
+        /// The series collection1
+        /// </summary>
+        private SeriesCollection seriesCollection1;
+
+        /// <summary>
+        /// The x formatter
+        /// </summary>
+        private Func<double, string> xFormatter;
+
         #endregion Fields
 
         #region Constructor
@@ -55,11 +70,66 @@ namespace CryostatControlClient.ViewModels
             this.compressorViewModel = new CompressorViewModel();
             this.he7ViewModel = new He7ViewModel();
             this.modusViewModel = new ModusViewModel();
+
+            this.InitSeriesCollection();
+
+            this.SeriesCollection[0].Values.Add(new DateTimePoint(DateTime.Now, 300));
+            this.SeriesCollection[1].Values.Add(new DateTimePoint(DateTime.Now, 5));
+            Thread.Sleep(1000);
+            this.SeriesCollection[0].Values.Add(new DateTimePoint(DateTime.Now, 50));
+            this.SeriesCollection[1].Values.Add(new DateTimePoint(DateTime.Now, 3));
+            Thread.Sleep(1000);
+            this.SeriesCollection[0].Values.Add(new DateTimePoint(DateTime.Now, 3));
+            this.SeriesCollection[1].Values.Add(new DateTimePoint(DateTime.Now, 0.8));
+            Thread.Sleep(1000);
+            this.SeriesCollection[0].Values.Add(new DateTimePoint(DateTime.Now, 1));
+            this.SeriesCollection[1].Values.Add(new DateTimePoint(DateTime.Now, 0.2));
         }
 
         #endregion Constructor
 
         #region Properties
+
+        /// <summary>
+        /// Gets or sets the series collection.
+        /// </summary>
+        /// <value>
+        /// The series collection.
+        /// </value>
+        public SeriesCollection SeriesCollection
+        {
+            get
+            {
+                Console.WriteLine("Getting seriescollection");
+                return this.seriesCollection1;
+                
+            }
+
+            set
+            {
+                this.seriesCollection1 = value;
+                Console.WriteLine("Setting seriescollection");
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the x formatter.
+        /// </summary>
+        /// <value>
+        /// The x formatter.
+        /// </value>
+        public Func<double, string> XFormatter
+        {
+            get
+            {
+                return this.xFormatter;
+            }
+
+            set
+            {
+                this.xFormatter = value;
+            }
+        }
 
         /// <summary>
         /// Gets the BlueforsViewModel.
@@ -115,6 +185,28 @@ namespace CryostatControlClient.ViewModels
             {
                 return this.modusViewModel;
             }
+        }
+
+        /// <summary>
+        /// Initializes the series collection.
+        /// </summary>
+        private void InitSeriesCollection()
+        {
+            this.SeriesCollection = new SeriesCollection
+                                        {
+                                            new LineSeries
+                                                {
+                                                    Title = "He7 Cooler",
+                                                    Values = new ChartValues<DateTimePoint>()             
+                                                },
+                                            new LineSeries
+                                                {
+                                                    Title = "Bluefors",
+                                                    Values = new ChartValues<DateTimePoint>()
+                                                }
+                                        };
+
+            this.xFormatter = val => new DateTime((long)val).ToString("HH:mm:ss");
         }
 
         #endregion Properties
