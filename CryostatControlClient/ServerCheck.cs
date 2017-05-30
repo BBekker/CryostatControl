db09b33a35c7735b5e76c42e4b5aaaa7e44c6e40
maikogoudriaan <M.Goudriaan-2@student.tudelft.nl>
2017-05-30 15:30:13 +0200

    initial commit for reconnecting the client

---
+++ CryostatControlClient/ServerCheck.cs
@@ -0,0 +1,57 @@
+﻿using CryostatControlClient.ServiceReference1;
+using CryostatControlClient.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CryostatControlClient
+{
+    public class ServerCheck
+    {
+
+        /// <summary>
+        /// The main application
+        /// </summary>
+        private App mainApp;
+
+        /// <summary>
+        /// The main window
+        /// </summary>
+        private MainWindow mainWindow;
+
+        private Timer timer;
+
+        private CommandServiceClient commandClient;
+
+        private DataClientCallback callbackClient;
+
+        public ServerCheck(App app, CommandServiceClient commandClient, DataClientCallback callbackClient)
+        {
+            this.mainApp = app;
+            this.mainWindow = this.mainApp.MainWindow as MainWindow;
+            this.commandClient = commandClient;
+            this.callbackClient = callbackClient;
+            this.timer = new Timer(this.CheckStatus, null, 100, 10000);
+        }
+
+        private void CheckStatus(object state)
+        {
+            try
+            {
+                if (commandClient.IsAlive())
+                {
+                    //commandClient.AmIRegisteredForData();
+                    //commandClient.AmIRegisteredForUpdates();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+    }
+}
