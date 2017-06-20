fbea3004c9afe80d8e226c58f68b7073ecf95f2e
Bernard Bekker <git@bernardbekker.nl>
2017-06-20 10:33:01 +0200

    update data enumerator

--- CryostatControlServer/Data/DataEnumerator.cs
+++ CryostatControlServer/Data/DataEnumerator.cs
@@ -13,44 +13,44 @@ namespace CryostatControlServer.Data
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1602:EnumerationItemsMustBeDocumented", Justification = "Reviewed. Suppression is OK here.")]
     public enum DataEnumerator
     {
-        LakePlate50K = 0,
-        LakePlate3K = 1,
+        LakePlate50K,
+        LakePlate3K,
 
-        ComWaterIn = 3,
-        ComWaterOut = 4,
-        ComHelium = 5,
-        ComOil = 6,
-        ComLow = 7,
-        ComLowAvg = 8,
-        ComHigh = 9,
-        ComHighAvg = 10,
-        ComDeltaAvg = 11,
+        ComWaterIn,
+        ComWaterOut,
+        ComHelium,
+        ComOil,
+        ComLow,
+        ComLowAvg,
+        ComHigh,
+        ComHighAvg,
+        ComDeltaAvg,
 
-        He3Pump = 12,
-        HePlate2K = 13,
-        HePlate4K = 14,
-        He3Head = 15,
-        He4Pump = 16,
-        He4SwitchTemp = 17,
-        He3SwitchTemp = 18,
-        He4Head = 19,
+        He3Pump,
+        HePlate2K,
+        HePlate4K,
+        He3Head,
+        He4Pump,
+        He4SwitchTemp,
+        He3SwitchTemp,
+        He4Head,
 
-        He3VoltActual = 20,
-        He4SwitchVoltActual = 21,
-        He3SwitchVoltActual = 22,
-        He4VoltActual = 23,
+        He3VoltActual,
+        He4SwitchVoltActual,
+        He3SwitchVoltActual,
+        He4VoltActual,
 
-        SensorAmount = 24,
+        SensorAmount,
 
-        HeConnectionState = 24,
-        ComConnectionState = 25,
-        LakeConnectionState = 26,
+        HeConnectionState = SensorAmount,
+        ComConnectionState,
+        LakeConnectionState,
 
-        ComError = 27,
-        ComWarning = 28,
-        ComHoursOfOperation = 29,
-        ComOperationState = 30,
+        ComError,
+        ComWarning,
+        ComHoursOfOperation,
+        ComOperationState,
 
-        DataLength = 31
+        DataLength
     }
 }
