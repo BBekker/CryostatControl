ef8907101749ef5a61f02b48f264f91a16707f19
maikogoudriaan <M.Goudriaan-2@student.tudelft.nl>
2017-07-06 19:19:13 +0200

    added new certificate

--- installation/network_setup.bat
+++ installation/network_setup.bat
@@ -2,5 +2,5 @@ certutil -p 0 -importPFX %~dp0certificate.pfx
 netsh http add sslcert ipport=0.0.0.0:18081 certhash=12ca606b7de59c926e65a20e4063d29befd0727a appid={4724405f-6333-4320-9896-8aaac233293a}
 netsh http add urlacl url=http://+:18080/ user=Everyone
 netsh http add urlacl url=https://+:18081/ user=Everyone
-certutil -p cryostat! -importPFX %~dp0ServerCertificate2.pfx
+certutil -p Cryostat! -importPFX %~dp0ServerCertificate.pfx
 pause
